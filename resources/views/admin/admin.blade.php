e0f69621ccaf92b7f1b55decd504a32b08972bf2
MosesDev <mosesdev@gmail.com>
2023-09-06 21:26:19 +0200

    nav bar

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -10,6 +10,31 @@
         <title>@yield('title') | Administration</title>
     </head>
     <body>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Agence</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Gerer les biens</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Gerer les option</a>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+
         <div class="container mt-5">
             @if(session('success'))
                 <div class="alert alert-success">

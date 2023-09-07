fff0bd6c9fe6df87aa099593d0c274a1f172b68c
MosesDev <mosesdev@gmail.com>
2023-09-07 10:53:28 +0200

    listing properties

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,10 +6,9 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <title>@yield('title') | Mon Agence </title>
+        <title>@yield('title', 'Mon Agence') </title>
     </head>
     <body>
-
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container-fluid">
               <a class="navbar-brand" href="#">Agence</a>
@@ -24,7 +23,7 @@
               <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a @class(['nav-link', 'active' => str_contains($route, 'property.')]) aria-current="page" href="/">Biens</a>
+                    <a @class(['nav-link', 'active' => str_contains($route, 'property.')]) aria-current="page" href="{{route('property.index')}}">Biens</a>
                   </li>
 
                 </ul>

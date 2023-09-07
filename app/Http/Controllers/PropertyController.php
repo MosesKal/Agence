187f12f5f00706f4e9c6d7542773b5b787b77e9f
MosesDev <mosesdev@gmail.com>
2023-09-07 18:21:01 +0200

    mail with laravel

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PropertyContactRequest;
 use App\Http\Requests\SearchPropertiesRequest;
+use App\Mail\PropertyContactMail;
 use App\Models\Property;
-use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Mail;
 
 class PropertyController extends Controller
 {
     public function index(SearchPropertiesRequest $request)
     {
-        $query = Property::query();
+        $query = Property::query()->orderBy('created_at', 'desc');
 
         if($price = $request->validated('price')){
             $query = $query->where('price', '<=', $price);
@@ -34,6 +35,21 @@ class PropertyController extends Controller
 
     public function show(string $slug, Property $property)
     {
+        $expectedSlug = $property->getSlug();
 
+        if($slug !== $expectedSlug){
+            return to_route('property.show', ['show' => $expectedSlug, 'property'=>$property]);
+        }
+
+        return view('property.show', [
+            'property' => $property
+        ]);
     }
+
+    public function contact(Property $property, PropertyContactRequest $request)
+    {
+        Mail::send(new PropertyContactMail($property, $request->validated()));
+
+        return back()->with('success', 'Votre demande de contact a bien ete envoye');
+    }  
 }
